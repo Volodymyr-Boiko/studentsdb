0a6655ce8b65744ba20a9f9414de5704c3440885
Volodymyr-Boiko <vbojko86@gmail.com>
2015-11-21 17:20:23 +0200

    new_branch

--- students/views.py
+++ students/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
 
-# Create your views here.
+
+def students_list(request):
+    return render(request, 'students_list.html', {})
