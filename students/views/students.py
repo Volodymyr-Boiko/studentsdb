2db76f611ea650c963afa84b57ee5cfa67165e79
Volodymyr-Boiko <vbojko86@gmail.com>
2015-11-24 08:48:36 +0200

    fdsfsdf

--- students/views/students.py
+++ students/views/students.py
@@ -2,21 +2,11 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from ..models import Students
 
 
 def students_list(request):
-    students = (
-        {'id': 1,
-         'first_name': u'Віталій',
-         'last_name': u'Подоба',
-         'ticket': 235,
-         'image': 'img/me.jpeg'},
-        {'id': 2,
-         'first_name': u'Корост',
-         'last_name': u'Андрій',
-         'ticket': 2123,
-         'image': 'img/piv.png'},
-    )
+    students = Students.objects.all()
     return render(request, 'students/students_list.html',
                   {'students': students})
 
